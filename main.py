844383d931b9342e5c23e0d8d922ba699934209b
Daniel Coady <daniel.coady@outlook.com.au>
2019-06-12 12:26:43 +1000

    added comments for some clarity

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ while images:
     i = images.pop(0)
     orig = cv2.imread(i) # read in the file we want to compare against
 
-    checkImages = deepcopy(images)
-    removed = []
+    checkImages = deepcopy(images) # get copy of list for our comparisons
+    removed = [] # used to track things we need to remove
     while checkImages:
         m = checkImages.pop(0)
         check = cv2.imread(m)
 
+        # similarity checking
         if orig.shape == check.shape and not(np.bitwise_xor(orig, check).any()):
             print(i + ' and ' + m + ' are the same')
             removed.append(m)
